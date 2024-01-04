b37c77d3a98e779dfff2cab7954f6000b8bdc9ea
abiyyu03 <abiyyu652@gmail.com>
2024-01-04 22:44:33 +0700

    first

---
+++ js/tailwind.config.js
@@ -0,0 +1,13 @@
+tailwind.config = {
+    theme: {
+        extend: {
+            colors: {
+                main: '#6096B4',
+                primary: '#93BFCF',
+                secondary: '#363062',
+                card: '#BDCDD6',
+                background: '#EEE9DA',
+            },
+        },
+    },
+};
