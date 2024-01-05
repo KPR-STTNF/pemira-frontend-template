ffe963dc660ab0486795298af6f7d1524a1fefbd
abiyyu03 <abiyyu652@gmail.com>
2024-01-05 18:06:58 +0700

    add more page : register, vote and add daisy ui

--- js/tailwind.config.js
+++ js/tailwind.config.js
@@ -1,13 +1,16 @@
 tailwind.config = {
-    theme: {
-        extend: {
-            colors: {
-                main: '#6096B4',
-                primary: '#93BFCF',
-                secondary: '#363062',
-                card: '#BDCDD6',
-                background: '#EEE9DA',
-            },
-        },
-    },
+	theme: {
+		extend: {
+			colors: {
+				main: '#6096B4',
+				primary: '#93BFCF',
+				secondary: '#363062',
+				card: '#BDCDD6',
+				background: '#EEE9DA',
+			},
+		},
+	},
+	daisyui: {
+		themes: false, //['light'],
+	},
 };
